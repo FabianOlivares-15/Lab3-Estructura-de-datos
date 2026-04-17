499b40ada390cd6a19f1fa6c7b168c04c904e609
Your Name <you@example.com>
2026-04-17 03:00:25 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -118,7 +118,7 @@ void eraseMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) {
     for(long i = 0; i < map->capacity; i++){
-        if(map->buckets[i]->key != NULL){
+        if(map->buckets[i] != NULL){
             map->current = i;
             return map->buckets[i];
         }
