d12c1c0687a527dc79e522c43e9af49d9c6556fb
Your Name <you@example.com>
2026-04-17 02:38:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -87,7 +87,7 @@ Pair * searchMap(HashMap * map,  char * key) {
     while(map->buckets[pos] != NULL){
         if(is_equal(key, map->buckets[pos]->key)){
             map->current = pos;
-            return map->buckets[pos]->value;
+            return map->buckets[pos];
         }
         pos = (pos + 1) % map->capacity;
     }
